52306b916521f8bd0e6f88841499eaaeb7fdc11e
Leon Henrik Plickat <leonhenrik.plickat@stud.uni-goettingen.de>
2022-03-22 20:44:17 +0100

    alphasort import.zig

--- import.zig
+++ import.zig
@@ -6,6 +6,6 @@
 
 pub const version = "0.1.0";
 
-pub const Term = @import("lib/Term.zig");
 pub const Attribute = @import("lib/Attribute.zig");
-const Event = @import("lib/event.zig").Event;
+pub const Event = @import("lib/event.zig").Event;
+pub const Term = @import("lib/Term.zig");
